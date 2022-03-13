c0dadcc3d76a9bf964f4f2976d5fa787b3868d97
Bassil-ali <baselali337@gmail.com>
2022-03-13 14:20:28 +0200

    setting teacher

---
+++ app/Http/Controllers/Teachers/dashboard/StudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Teachers\dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use App\Models\Attendance;
+use App\Models\Section;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StudentController extends Controller
+{
+
+    public function index()
+    {
+        $ids= DB::table('teacher_section')->where('teacher_id',auth()->user()->id)->pluck('section_id');
+        $students = Student::whereIn('section_id',$ids)->get();
+        return view('pages.Teachers.dashboard.students.index',compact('students'));
+    }
+
+    public function sections()
+    {
+        $ids = DB::table('teacher_section')->where('teacher_id', auth()->user()->id)->pluck('section_id');
+        $sections = Section::whereIn('id', $ids)->get();
+        return view('pages.Teachers.dashboard.sections.index', compact('sections'));
+    }
+
+    public function attendance(Request $request)
+    {
+
+        try {
+
+            $attenddate = date('Y-m-d');
+            $classid = $request->section_id;
+            foreach ($request->attendences as $studentid => $attendence) {
+
+                if ($attendence == 'presence') {
+                    $attendence_status = true;
+                } else if ($attendence == 'absent') {
+                    $attendence_status = false;
+                }
+
+                Attendance::create([
+                    'student_id' => $studentid,
+                    'grade_id' => $request->grade_id,
+                    'classroom_id' => $request->classroom_id,
+                    'section_id' => $request->section_id,
+                    'teacher_id' => 1,
+                    'attendence_date' => $attenddate,
+                    'attendence_status' => $attendence_status
+                ]);
+            }
+            toastr()->success(trans('messages.success'));
+            return redirect()->back();
+        } catch (Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+
+    public function editAttendance(Request $request){
+
+        try{
+            $date = date('Y-m-d');
+            $student_id = Attendance::where('attendence_date',$date)->where('student_id',$request->id)->first();
+            if( $request->attendences == 'presence' ) {
+                $attendence_status = true;
+            } else if( $request->attendences == 'absent' ){
+                $attendence_status = false;
+            }
+            $student_id->update([
+                'attendence_status'=> $attendence_status
+            ]);
+            toastr()->success(trans('messages.success'));
+            return redirect()->back();
+        }
+        catch (\Exception $e){
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+
+    }
+
+}
--- app/Models/Section.php
+++ app/Models/Section.php
@@ -27,5 +27,9 @@ class Section extends Model
      {
          return $this->belongsToMany('App\Models\Teacher','teacher_section')->select('Name');
      }
+      public function Grades()
+    {
+        return $this->belongsTo('App\Models\Grade','Grade_id');
+    }
 
 }
