a918406eb2a17ad08fc64d566ea4e491090aa8f5
Bassil-ali <baselali337@gmail.com>
2022-04-05 20:57:17 +0200

    attendance report

--- public/assets/js/datepicker.js
+++ public/assets/js/datepicker.js
@@ -12,15 +12,18 @@ NOTE:
   "use strict";
 // Line Stacked
 
-    $('.date-picker-default').datepicker({
-        format: 'mm-dd-yyyy',
-        autoclose: true,
-        orientation: "bottom",
-        templates: {
-            leftArrow: '<i class="fa fa-angle-left"></i>',
-            rightArrow: '<i class="fa fa-angle-right"></i>'
-        }
-    });
+     $('.date-picker-default').datepicker({
+         format: 'yyyy-mm-dd',
+         todayBtn: "linked",
+         clearBtn: true,
+         autoclose: true,
+         todayHighlight: true,
+         orientation: "bottom",
+         templates: {
+             leftArrow: '<i class="fa fa-angle-left"></i>',
+             rightArrow: '<i class="fa fa-angle-right"></i>'
+         }
+     });
 
 
     $('.display-years').datepicker({
